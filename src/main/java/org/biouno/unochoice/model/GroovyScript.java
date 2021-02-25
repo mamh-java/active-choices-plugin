a00c25de2c13117a5baf139e7e12a7527417e07a
bright.ma RMSH06 <bright.ma@blackshark.com>
2021-02-25 18:18:45 +0800

    src: java: add @Symbol to support pipeline
    add @Symbol to
    CascadeChoiceParameter.java
    ChoiceParameter.java
    DynamicReferenceParameter.java
    
    GroovyScript.java
    ScriptlerScript.java
    
    Signed-off-by: bright.ma RMSH06 <bright.ma@blackshark.com>

--- src/main/java/org/biouno/unochoice/model/GroovyScript.java
+++ src/main/java/org/biouno/unochoice/model/GroovyScript.java
@@ -37,6 +37,7 @@ import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
 import org.biouno.unochoice.util.SafeHtmlExtendedMarkupFormatter;
+import org.jenkinsci.Symbol;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript;
 import org.jenkinsci.plugins.scriptsecurity.scripts.ApprovalContext;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -307,6 +308,7 @@ public class GroovyScript extends AbstractScript {
     // --- descriptor
 
     @Extension
+    @Symbol({"groovyScript"})
     public static class DescriptorImpl extends ScriptDescriptor {
         /*
          * (non-Javadoc)
